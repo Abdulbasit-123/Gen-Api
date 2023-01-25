6f3f85ceab167919af3d6ba95a49b6d1799168d4
Abdulbasit-123 <bodundeabdulbasit802@gmail.com>
2023-01-25 21:57:37 +0100

    jwt token

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,7 +1,22 @@
 
+const jwt = require('jsonwebtoken')
+const   CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
-    res.send('Fake Login/Register/Signup Route')
+    const {username, password} = req.body
+    
+    if(!username || !password){
+    throw new CustomAPIError('Please provide email and password', 400)
+    }
+
+// normally provided by DB
+    const id = new Date().getDate()
+
+// keeping payload small helps user have betterf experience
+// use long, complex and unguessable string values
+    const token = jwt.sign({id, username}, process.env.JWT_SECRET, {expiresIn: '30d'})
+
+    res.status(200).json({msg: 'user created',token})
 }
 
 const dashboard = async (req, res) => {
