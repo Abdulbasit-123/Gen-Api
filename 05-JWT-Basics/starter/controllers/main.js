6ce809a0a62c4ae17ec9a9e6c6e9696472466a96
Abdulbasit-123 <bodundeabdulbasit802@gmail.com>
2023-01-28 22:30:16 +0100

    jwt auth five

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,6 +1,6 @@
 
 const jwt = require('jsonwebtoken')
-const   CustomAPIError = require('../errors/custom-error')
+const  CustomAPIError = require('../errors/custom-error')
 
 const login = async (req, res) => {
     const {username, password} = req.body
@@ -20,24 +20,17 @@ const login = async (req, res) => {
 }
 
 const dashboard = async (req, res) => {
-    const authHeader = req.headers.authorization
+    console.log(req.user)
 
-    if(!authHeader || !authHeader.startsWith('Bearer ')){
-        throw new CustomAPIError('No token provided', 401)
-    }
-
-const token = authHeader.split(' ')[1]
-
-try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET)
 
     const luckyNumber = Math.floor(Math.random() * 100)
 
-    res.status(200).json({msg: `Hello, ${decoded.username}`, secret: `Here is your authorized data, your lucky number is ${luckyNumber} `})
+    res
+    .status(200)
+    .json({msg: `Hello, ${req.user.username}`, 
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber} `})
 
-} catch (error) {
-    throw new CustomAPIError('Not authorized to acess this route', 401)
-}
+
 }
 
 
